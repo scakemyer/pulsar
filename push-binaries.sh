fa7357904babd8f436353575bea3579d8d664bdb
Charly <charlyfb90@gmail.com>
2017-10-12 22:28:03 +0200

    Update push-binaries.sh

--- push-binaries.sh
+++ push-binaries.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # Push binaries to quasar-binaries repo
 make binaries
-echo The gh_token is $GH_TOKEN
 cd binaries && git remote add binaries https://$GH_TOKEN@github.com/charly3pins/quasar-binaries
 git push binaries master
 if [ $? -ne 0 ]; then
